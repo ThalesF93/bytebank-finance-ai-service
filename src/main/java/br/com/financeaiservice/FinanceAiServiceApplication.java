b46bb27925ed3b258a7d542cc39289067315648c
Thales Fernandes <thalesgarcezf@gmail.com>
2026-06-10 11:52:44 -0300

    first commit

---
+++ src/main/java/br/com/financeaiservice/FinanceAiServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.financeaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceAiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/financeaiservice/FinanceAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financeaiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
